45f65957e6bc276bfaca62a3634332fed0bec1d5
Morgan Henning <megannh6349@gmail.com>
2022-09-17 00:14:07 -0400

    inital commit, canvas with black box representing player

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import ReactRogue from "./ReactRogue";
+
+const App = () => (
+  <div className="App">
+    <ReactRogue width={40} height={40} tilesize={16}></ReactRogue>
+  </div>
+);
+
+export default App;
