73b05ab75e0b1d41ebed6301aabdf5c7aa291a26
Morgan Henning <megannh6349@gmail.com>
2022-09-19 17:04:03 -0400

    stable, player is represented using @ symbol and able to travel around procedurally generated map with no constraints

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -1,15 +1,17 @@
 import React, { useRef, useEffect, useState } from "react";
 import InputManager from "./InputManager";
 import Player from "./Player";
+import World from "./World";
 
 const ReactRogue = ({ width, height, tilesize }) => {
   const canvasRef = useRef();
-  const [player, setPlayer] = useState(new Player(1, 2, tilesize));
+  //const [player, setPlayer] = useState(new Player(1, 2, tilesize));
+  const [world, setWorld] = useState(new World(width, height, tilesize));
   let inputManager = new InputManager();
   const handleInput = (action, data) => {
     console.log(`handle input: ${action}:${JSON.stringify(data)}`);
-    let newPlayer = new Player();
-    Object.assign(newPlayer, player);
+    let newWorld = new World();
+    Object.assign(newWorld, world);
     newPlayer.move(data.x, data.y);
     setPlayer(newPlayer);
   };
@@ -28,6 +30,7 @@ const ReactRogue = ({ width, height, tilesize }) => {
     console.log("Draw to canvas");
     const ctx = canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+    world.draw(ctx);
     player.draw(ctx);
   });
   return (
